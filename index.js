4c41def12b9132ab070b82c7008096189a6a07fc
swirix00 <158514707+swirix00@users.noreply.github.com>
2024-02-01 21:27:15 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["💫","rp commandy","cloudrp bot"];
 
 
 let currentIndex = 0;
